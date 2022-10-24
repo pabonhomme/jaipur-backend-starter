d8f0c56e14b0b4f515aefa20e488f5d3b135e1de
pabonhomme1 <pabonhomme1@a112pc14L.local.isima.fr>
2022-10-24 17:48:12 +0200

    ajout des tests corrigés

--- src/services/gameService.test.js
+++ src/services/gameService.test.js
@@ -1,38 +1,34 @@
 import * as gameService from "./gameService"
 
 describe("Game service", () => {
-  test("should put camels from hand to herd", () => {
-    const game = { "_players": [
-      { 
-          "hand": ["cloth", "silver", "diamonds", "camel"],
-          "camelsCount": 2, 
-          "score": 0
-      },
-      {
-          "hand": ["gold", "gold", "leather", "diamonds", "spice"],
-          "camelsCount": 0,
-          "score": 0
-      }]}
-
-      gameService.putCamelsFromHandToHerd(game)
-
-      expect(game._players[0].hand.length).toBe(3)
-      expect(game._players[0].camelsCount).toBe(3)
-
+  test("should init a deck", () => {
+    const defaultDeck = gameService.initDeck()
+    expect(defaultDeck.length).toBe(52)
+    expect(defaultDeck.filter((card) => card === "diamonds").length).toBe(6)
+    // etc
   })
 
   test("should draw cards", () => {
-    let deck = ["gold", "gold", "gold", "gold"]
-
-    let drawncards = gameService.drawCards(deck, 2)
-    expect(drawncards).toStrictEqual(["gold", "gold"])
-    expect(deck).toStrictEqual(["gold", "gold"])
-
+    const deck = ["camel", "diamonds", "gold"]
+    const drawnCard = gameService.drawCards(deck, 1)
+    expect(deck.length).toBe(2)
+    expect(drawnCard).toStrictEqual(["camel"])
   })
 
-  test("should init a deck", () => {
-    let deck = gameService.initDeck()
-    expect(deck.length).toBe(52)
+  test("should put camels from hand to herd", () => {
+    const game = {
+      _players: [
+        { hand: ["camel", "gold"], camelsCount: 0 },
+        { hand: ["gold", "gold"], camelsCount: 0 },
+      ],
+    }
+    gameService.putCamelsFromHandToHerd(game)
+    expect(game._players[0].hand.length).toBe(1)
+    expect(game._players[0].hand).toStrictEqual(["gold"])
+    expect(game._players[0].camelsCount).toBe(1)
 
+    expect(game._players[1].hand.length).toBe(2)
+    expect(game._players[1].hand).toStrictEqual(["gold", "gold"])
+    expect(game._players[1].camelsCount).toBe(0)
   })
 })
